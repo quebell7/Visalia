87023045081686b00849ba68b47eb7f9687cf391
quebell7 <kbell.marinescu@gmail.com>
2016-04-20 22:16:28 -0700

    4.20.changes

--- js/index.js
+++ js/index.js
@@ -17,13 +17,28 @@ $(".navline").hover(function showRollover() {
     $(this).removeClass("rollover");
 } );
 
+
 $(".mobile-nav-button").click(mobileNavToggle);
 	
 	function mobileNavToggle() {
 		$(".mobile-nav").slideToggle();
-		// $(this)addClass("close");
+		$(".line").toggleClass("active");
+		$(".orders, #logo").hide();
 	}
 
-		
-
 }
+
+
+
+
+// function clickHandler() {
+//     $('#hide').toggle();
+//     $('#show').toggle();
+//     $('.orders').toggle();
+// }
+    
+// $(document).ready(function(){
+//     $('#show').hide();
+    
+//     $('#hide, #show').on('click', clickHandler);
+// });
